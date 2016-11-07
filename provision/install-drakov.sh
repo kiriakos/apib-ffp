0c22bf1d67b856cbf7352ad0e184703187eca07e
Kiriakos Krastillis <kiriakos@glanzkinder.de>
2016-11-07 09:03:37 +0100

    Drakov container

--- provision/install-drakov.sh
+++ provision/install-drakov.sh
@@ -3,8 +3,8 @@
 cd /vagrant/infra/drakov/
 docker build -t apibffp/drakov .
 
-docker run -d --name drakov -p 10180:10180 -v /vagrant:/ffp \
-    apibffp/drakov -f /ffp/ffp.apib
+docker run -d --name drakov -p 10180:3000 -v /vagrant:/ffp \
+    apibffp/drakov -f /ffp/ffp.apib --watch
 
 cp /vagrant/infra/drakov/*.service /etc/systemd/system/
 systemctl enable drakov.service
